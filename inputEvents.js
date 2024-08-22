6b1d6cd3e2a0a6fe7224d2df53c3d25b0ec50333
Maxwell Warren <95433166+Valereon@users.noreply.github.com>
2024-08-22 10:32:01 -0400

    remove debug statements in "inputEvents.js"

--- inputEvents.js
+++ inputEvents.js
@@ -2,19 +2,15 @@ function getInputLoop() {
     addEventListener("keydown", function (event) {
         if (event.key == "w") {
             movePlayer("N")
-            this.alert("North")
         }
         if (event.key == "s") {
             movePlayer("S")
-            this.alert("south")
         }
         if (event.key == "d") {
             movePlayer("E")
-            this.alert("east")
         }
         if (event.key == "a") {
             movePlayer("W")
-            this.alert()
         }
     });
 }
